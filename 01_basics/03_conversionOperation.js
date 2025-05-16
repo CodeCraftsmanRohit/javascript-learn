2e3c24978e296c75ed7887305f73f4039b4274af
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-16 09:38:39 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+console.log(typeof (valueInNumber))
+console.log( valueInNumber) //NaN -> if kisi number me convert nhi ho skta
+
+//"33"=>33
+//"33abs"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1
+ isLoggedIn="rohit"
+ isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+//1=>true ; 0=> false
+// ""=>false
+// "hitesh" => true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
