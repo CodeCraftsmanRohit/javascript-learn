a8f28bbb2a1e02113f04cef976cb78d774c527b1
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-16 09:11:56 +0000

    dataTypeIntro

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using nodejs, not browser
+// console.log(3+3); console.log("Hitesh")
+console.log(typeof NaN)//number
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+//PRIMITIVE
+
+//number => 2 to power 53 -1
+// bigint
+//string=>""
+//boolean=> true/false
+//null => standalone value
+//undefine=>
+//symbol   => unique
+
+
+//object
+console.log(typeof "hitesh")
