0028aa3da80deaaf19c2f14c30abb3df1eeabff7
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-16 09:55:41 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,3 +29,41 @@ let someNumber =33
 let stringNumber =String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// *********************** OPERATIONS *******************
+
+let value =3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2="hitesh"
+
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+console.log(+true) // 9 laga kar data type convert hua h from boolean to int
+console.log(-"") // 9 laga kar data type convert hua h from boolean to int
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+++gameCounter
+console.log(gameCounter)
+
+//prefix and postfix
