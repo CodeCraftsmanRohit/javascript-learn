9212494ad952cc888cad887dc2fcf80ba195320e
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-16 07:59:08 +0000

    let,var,const

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const acountId=144553;
+let accountEmail="rohit@gmail.com"
+var ccountpassword="12345"
+accountCity="jaipur"
+ 
+// acountId=5;
+console.log(acountId)
+accountEmail="hc@gmail.com"
+ccountpassword=753
+accountCity="patna"
+let accountstate;
+console.table([acountId,accountEmail,ccountpassword,accountCity,accountstate])
+
+/*
+perfer not to use var 
+bcz of issue in block scope and functional scope
+ */
