a6f0f2077906884574b837f3a94e0963fa0f83b3
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-19 11:06:41 +0000

    function intro

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -51,6 +51,8 @@ function handleObject(anyobject){
     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
 }
 
+
+
 // handleObject(user)
 handleObject({
     username: "sam",
