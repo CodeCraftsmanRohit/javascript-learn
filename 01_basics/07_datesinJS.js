cf37ef782bd6c2f148d08e5f34d26726fdfe39ad
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-19 09:00:46 +0000

    Date and Time

---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,14 @@
+// let mydate=new Date()
+// console.log(mydate)
+// console.log(mydate.toDateString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleTimeString())
+// console.log(mydate.toLocaleString())
+// console.log(typeof mydate)
+
+
+// let myTImeStamp =Date.now()
+
+// console.log(myTImeStamp) //milli second
+let myCreatedDate=new Date("03-16-2024")
+console.log(myCreatedDate.getTime())
