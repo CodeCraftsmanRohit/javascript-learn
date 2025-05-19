6d5b0d4c28aba8d8b2add65890b0550a8eb12f1a
CodeCraftsmanRohit <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-19 10:28:26 +0000

    Objects and its uses

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,33 @@
+// const tinderUser=new Object()    --> ye ek singleton object h!!
+// const tinderUser={}  // --> ye ek non single ton object hai
+
+// tinderUser.id="132"
+// tinderUser.name="johhnyy"
+// tinderUser.isLoggedIn=false;
+// console.log(tinderUser)
+
+// const regularUser={
+//     email:"abc@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"rohit",
+//             lastname:"choudhary"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+// obj3={obj1,obj2}
+// console.log(obj3)
+// const obj3=Object.assign({},obj1,obj2)  //target,source
+
+const obj3={...obj1,...obj2}
+console.log(obj3)
+console.log(Object.keys(obj3))
+console.log(Object.values(obj3))
+console.log(Object.entries(obj3))
+
+console.log(obj3.hasOwnProperty('3'))
