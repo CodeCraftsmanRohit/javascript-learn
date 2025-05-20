2603f22b65e5634cdfcc70f3bf44e38b7215cc6a
Rohit Kumar <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-20 03:23:53 +0000

    SCOPE

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,20 @@
+let a=10
+// const b=20
+// var c=300
+
+
+if(true){
+    let a=50
+    const b=20
+    //  c=30
+    console.log(a);
+
+}
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// chrome ke console me variable declare krnge to vo alag h and code-editor me node ke through jab run krnge tb vo alag h
