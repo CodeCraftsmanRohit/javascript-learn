12eac74a04898442371af56ab70b65053198c64a
Rohit Kumar <166098901+CodeCraftsmanRohit@users.noreply.github.com>
2025-05-20 08:42:27 +0000

    scope and closure

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -18,3 +18,48 @@ console.log(a);
 
 
 // chrome ke console me variable declare krnge to vo alag h and code-editor me node ke through jab run krnge tb vo alag h
+
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
